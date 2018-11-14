e9d6af371625392b2c720b9cf17a38bf9a39c96e
tadoogie <tim.duguid@glasgow.ac.uk>
2018-11-14 16:25:39 +0000

    Upload sample

---
+++ js/index-unforgiven.js
@@ -0,0 +1,29 @@
+/* This script loads the MEI file via HTTP request */
+///////////////////////////
+/* Some global variables */
+///////////////////////////
+var vrvToolkit = new verovio.toolkit();
+var zoom = 30;
+var pageHeight = 2970;
+var pageWidth = 2100;
+
+///////////////////////////////////////////////////
+/* A function for setting options to the toolkit */
+///////////////////////////////////////////////////
+function setOptions() {
+    //////////////////////////////////////////////////////////////
+    /* Adjust the height and width according to the window size */
+    //////////////////////////////////////////////////////////////
+    pageHeight = $(document).height() * 100 / zoom ;
+    pageWidth = $(window).width() * 100 / zoom ;
+    options = {
+                pageHeight: pageHeight,
+                pageWidth: pageWidth,
+                scale: zoom,
+                adjustPageHeight: true
+            };
+    vrvToolkit.setOptions(options);
+}
+
+/* Show the score */
+showScore('score', 'data/the_unforgiven.mei') // <-- CHANGE THIS TO YOUR FILE
